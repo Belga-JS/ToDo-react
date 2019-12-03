2bc33fe9991f1007389b1f32888ba1d362a2b08e
Belga-JS <Meherzi.belga@yahoo.com>
2019-12-03 10:34:20 +0100

    Updates

---
+++ precache-manifest.ab54fcb2ead7257a430b975f3587dcfb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e20247e1182687cd06364346b13bf43e",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "a23c1d09297b60ec6e7d",
+    "url": "/todo-react/static/css/main.86788c9f.chunk.css"
+  },
+  {
+    "revision": "0fdfef790264b906c6ad",
+    "url": "/todo-react/static/js/2.1357b137.chunk.js"
+  },
+  {
+    "revision": "a23c1d09297b60ec6e7d",
+    "url": "/todo-react/static/js/main.7710a5d8.chunk.js"
+  },
+  {
+    "revision": "fe0a199e7628ae13b5e6",
+    "url": "/todo-react/static/js/runtime-main.dbefe885.js"
+  }
+]);
